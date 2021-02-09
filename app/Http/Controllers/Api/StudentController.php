e89a9c24c6e263bba321f239ab325a4a90032b9d
SakilAhmed007 <sakilhossain01969@gmail.com>
2021-02-09 21:16:46 +0600

    save data with api

--- app/Http/Controllers/Api/StudentController.php
+++ app/Http/Controllers/Api/StudentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\StudenClass;
 use Illuminate\Http\Request;
 
 class StudentController extends Controller
@@ -14,7 +15,8 @@ class StudentController extends Controller
      */
     public function index()
     {
-        //
+        $studentClass = StudenClass::all();
+        return $studentClass;
     }
 
     /**
@@ -35,7 +37,8 @@ class StudentController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $students = StudenClass::create($request->all());
+        return 'done';
     }
 
     /**
